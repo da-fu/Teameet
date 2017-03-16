54ec51a7bdc1a9bb0c5b52c91e3ccf6191097cac
Da Fu <dall8877856@aol.com>
2017-03-16 18:05:23 -0400

    login finished

--- server_phase3/server.js
+++ server_phase3/server.js
@@ -19,7 +19,10 @@ app.use(session({
   secret: 'teameetSession',
   resave: true,
   saveUninitialized: true,
-  cookie: {secure: false, maxAge: 300000}
+  cookie: {
+    secure: false,
+    maxAge: 300000
+  }
 }));
 
 app.use(bodyParser.json());
@@ -46,11 +49,11 @@ var mailServer = nodemailer.createTransport({
 mongoose.connect('mongodb://localhost/teameet-test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'database: connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log('database: connection established');
 });
 
-var userSchema=mongoose.Schema({
+var userSchema = mongoose.Schema({
   status: String,
   givenName: String,
   familyName: String,
@@ -63,18 +66,18 @@ var userSchema=mongoose.Schema({
 });
 var user = mongoose.model('user', userSchema);
 
-var profileSchema=mongoose.Schema({
+var profileSchema = mongoose.Schema({
 
 });
 var profile = mongoose.model('profile', profileSchema);
 
-var settingSchema=mongoose.Schema({
+var settingSchema = mongoose.Schema({
 
 });
 var setting = mongoose.model('setting', settingSchema);
 
 
-var groupSchema=mongoose.Schema({
+var groupSchema = mongoose.Schema({
   university: String,
   course: String,
   section: String,
@@ -84,10 +87,10 @@ var groupSchema=mongoose.Schema({
   groupName: String,
   groupId: Number
 });
-var group = mongoose.model('group',groupSchema);
+var group = mongoose.model('group', groupSchema);
 
 
-var courseSchema=mongoose.Schema({
+var courseSchema = mongoose.Schema({
   university: String,
   instructor: String,
   courseCode: String,
@@ -97,19 +100,19 @@ var courseSchema=mongoose.Schema({
   deadline: Date,
   requirements: String,
   deadlinePassed: Boolean,
-  createTime:Date
+  createTime: Date
 });
 var course = mongoose.model('course', courseSchema);
 
 
-var membershipSchema=mongoose.Schema({
+var membershipSchema = mongoose.Schema({
   studentEmail: String,
   groupID: String
 });
 var membership = mongoose.model('membership', membershipSchema);
 
 
-var messageSchema=mongoose.Schema({
+var messageSchema = mongoose.Schema({
   title: String,
   time: Date,
   sender: String,
@@ -121,7 +124,7 @@ var message = mongoose.model('message', messageSchema);
 
 //////////////////////////////////////////////// Routers /////////////////////////////////////////////////
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
   /*
   var newUser = new user({
     status: "String",
@@ -142,33 +145,49 @@ app.get('/', function(req,res){
   res.render('index');
 });
 
-app.get('/TermsOfUse', function(req,res){
+app.get('/join-team', function (req, res) {
+  res.render('join-team');
+});
+
+app.get('/student-profile', function (req, res) {
+  res.render('student-profile');
+});
+
+app.get('/student-setting', function (req, res) {
+  res.render('student-setting');
+});
+
+
+
+app.get('/TermsOfUse', function (req, res) {
   res.render('Terms_Of_Use');
 });
-app.get('/test', function(req,res){
-  user.find({}, function(err, users) {
+app.get('/test', function (req, res) {
+  user.find({}, function (err, users) {
     if (err) throw err;
-    res.render('test', {users:users});    
+    res.render('test', {
+      users: users
+    });
   });
 });
 
-app.get('/PrivacyPolicy', function(req,res){
+app.get('/PrivacyPolicy', function (req, res) {
   res.render('privacy_policy');
 });
 
-app.get('/login', function(req,res){
+app.get('/login', function (req, res) {
   res.render('login');
 });
 
-app.post('/checkLogin', function(req,res){
+app.post('/checkLogin', function (req, res) {
 
 });
 
-app.get('/create-team', function(req,res){
+app.get('/create-team', function (req, res) {
   res.render('create-team');
 });
 
-app.post('/checkteam', function(req,res){  
+app.post('/checkteam', function (req, res) {
   var newTeam = new group({
     university: "UofT",
     course: req.body.course,
@@ -179,23 +198,23 @@ app.post('/checkteam', function(req,res){
     groupName: req.body.groupname,
     groupId: 1
   });
-  newTeam.save(function(err) {
+  newTeam.save(function (err) {
     if (err)
       throw err;
     console.log('Team created!');
-    
+
   });
-   res.redirect("/result")
+  res.redirect("/join-team")
 });
 
-app.get('/courses-create', function(req,res){
+app.get('/courses-create', function (req, res) {
   res.render('courses-create');
 });
 
 
-app.post('/checkCourse', function(req,res){
+app.post('/checkCourse', function (req, res) {
 
-  
+
   var newCourse = new course({
     university: "uoft",
     instructor: "bg",
@@ -206,60 +225,91 @@ app.post('/checkCourse', function(req,res){
     deadline: new Date(),
     requirements: "String",
     deadlinePassed: true,
-    createTime:new Date()
+    createTime: new Date()
   });
-  newCourse.save(function(err) {
+  newCourse.save(function (err) {
     if (err)
       throw err;
     console.log('Course created!');
-    
+
   });
-   res.redirect("/select")
+  res.redirect("/select")
 });
 
-app.get('/select', function(req,res){
-      course.find({}, function(err, courses) {
-        if (err) throw err;
-        res.render('select', {courses:courses});    
-      });
+app.get('/select', function (req, res) {
+  course.find({}, function (err, courses) {
+    if (err) throw err;
+    res.render('select', {
+      courses: courses
+    });
+  });
 });
 
-app.get('/result', function(req,res){
-  
-      group.find({}, function(err, groups) {
-        if (err) throw err;
-        res.render('result', {groups:groups});    
-      });
-      
-      
-});
+// app.get('/result', function(req,res){
 
-app.get('/registration_step1', function(req,res){
+//       group.find({}, function(err, groups) {
+//         if (err) throw err;
+//         res.render('result', {groups:groups});    
+//       });
+
+
+// });
+
+app.get('/registration_step1', function (req, res) {
   res.render('registration_step1');
 });
 
-app.post('/checkReg', function(req,res){
+app.post('/logincheck', function (req, res) {
+  var login_email = req.body.email;
+  var login_password = req.body.password;
+  user.find({}, function (err, users) {
+    if (err) throw err;
+    users.forEach(function (i) {
+      if (i.email == login_email && i.password == login_password) {
+        if (i.status == "Student") {
+          course.find({}, function (err, courses) {
+            if (err) throw err;
+            res.render('select', {
+              courses: courses,
+              login_id: i
+            });
+          });
+        } else {
+          res.render("instructor", {
+            login_id: i
+          })
+        }
+      }
+    });
+  });
 
-  var userStatusCache=req.body.status;
+});
+
+
+app.post('/checkReg', function (req, res) {
+
+  var userStatusCache = req.body.status;
   //console.log(userStatus);
-  var givenNameCache=req.body.givenName;
+  var givenNameCache = req.body.givenName;
   //console.log(givenName);
-  var familyNameCache=req.body.familyName;
+  var familyNameCache = req.body.familyName;
   //console.log(familyName);
-  var emailAddr=req.body.email;
+  var emailAddr = req.body.email;
   //console.log(emailAddr);
-  var passwordCache=req.body.password;
+  var passwordCache = req.body.password;
   //console.log(password);
-  var securityQuestionCache=req.body.securityQuestion;
+  var securityQuestionCache = req.body.securityQuestion;
   //console.log(securityQuestion);
-  var securityAnswerCache=req.body.securityAnswer;
+  var securityAnswerCache = req.body.securityAnswer;
   //console.log(securityAnswer);
 
-  var emailFlag=false;
+  var emailFlag = false;
 
-  user.find({ email:emailAddr }, function(err,user) {
-    if (user.email==emailAddr)
-        emailFlag=true;
+  user.find({
+    email: emailAddr
+  }, function (err, user) {
+    if (user.email == emailAddr)
+      emailFlag = true;
   });
 
 
@@ -274,11 +324,11 @@ app.post('/checkReg', function(req,res){
     // "activated" will be set to false by default until the SMTP middleware is in service
     activated: true
   });
-  newUser.save(function(err) {
+  newUser.save(function (err) {
     if (err)
       throw err;
     console.log('User created!');
-    
+
   });
 
   //This part can only be tested once the website is registered with a specific domain.
@@ -315,48 +365,63 @@ app.post('/checkReg', function(req,res){
 
 });
 
-app.get('/registration_step2', function(req,res){
+app.post('/checkresult', function (req, res) {
+  var course = req.body.course;
+  //res.render('result', {coursenum:course});
+  group.find({}, function (err, groups) {
+    if (err) throw err;
+    res.render('result', {
+      groups: groups,
+      coursenum: course
+    });
+  });
+
+  //  res.redirect("/result")
+});
+
+
+
+app.get('/registration_step2', function (req, res) {
   res.render('registration_step2');
 });
 
 
 // use regular expression to handle the GET query
-app.get('/xxxxxxxxxx', function(req,res){
-  if(req.query.method =="activation"){
-    user.find({ _id:req.query.userID }, function(err,user) {
+app.get('/xxxxxxxxxx', function (req, res) {
+  if (req.query.method == "activation") {
+    user.find({
+      _id: req.query.userID
+    }, function (err, user) {
       if (err)
         throw err
-      else{
-        if (user.activated===false){
-          req.session.userEmail=user.email;
+      else {
+        if (user.activated === false) {
+          req.session.userEmail = user.email;
           res.render('registration_step3');
-        }
-        else{
+        } else {
           res.send("This account has been activated!");
         }
       }
     });
-  }
-  else{
+  } else {
     res.send("The page requested is invalid!");
   }
 });
 
 
-app.get("/dashboardPrep", function(req,res){
+app.get("/dashboardPrep", function (req, res) {
   if (req.session.loggedIn) {
-    if(req.session.status=="instructor")
+    if (req.session.status == "instructor")
       res.redirect('/instructor_welcome');
 
-    else if(req.session.status=="student")
+    else if (req.session.status == "student")
       res.redirect('/student_welcome');
 
     else
       res.redirect('/admin_welcome')
 
 
-  }
-  else
+  } else
     res.redirect('/');
 });
 
